c0151ea6fbbe001bd2fb668d1acb81dd6bcba1aa
shotaro-ada <syouta2223505@gmail.com>
2023-05-20 11:12:50 +0000

    map created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
-import GoogleMapComponent from "./components/GoogleMaps";
+import GoogleMapComponent from "./components/GoogleMapComponent";
 import { Route, Routes } from "react-router-dom";
 import MapPage from "./pages/MapPage";
+import { GoogleMap } from "@react-google-maps/api";
 
 function App() {
+  console.log("test")
   return (
+    // <GoogleMapComponent
+    //   pos={{
+    //     lat: 43.048225,
+    //     lng: 141.49701,
+    //   }}
+    // />
     <Routes>
-      <Route path="/mapPage" element={<MapPage />} />
+      <Route path="/" element={<MapPage />} />
     </Routes>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Header = () => {
+  return (
+    <StyledDiv>
+      <H2>座標を当てろ</H2>
+    </StyledDiv>
+  );
+};
+
+const StyledDiv = styled.div`
+  width: 100%;
+  height: 72px;
+  background-color: red;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+  color: white;
+  margin: 0;
+`;
+
+export default Header;
