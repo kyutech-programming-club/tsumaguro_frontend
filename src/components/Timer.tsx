69d1037f159478c7b8d06016708535fbc9ee4fb3
shotaro-ada <syouta2223505@gmail.com>
2023-05-20 17:31:50 +0000

    complete game rule

---
+++ src/components/Timer.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+type Params = {
+  isCounting: boolean;
+  whenZero: () => void;
+};
+
+const waitTime = 10
+
+const TimerComponent = (props: any) => {
+  const [value, setValue] = useState(waitTime);
+
+  useEffect(() => {
+    let intervalId: any = null;
+
+    if (props.isCounting) {
+      intervalId = setInterval(() => {
+        setValue((prevValue) => prevValue - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [props.isCounting]);
+
+  useEffect(() => {
+    if (value === 0) {
+      props.whenZero("this is zero");
+    }
+  }, [value]);
+
+  return <div>{value}</div>;
+};
+
+export default TimerComponent;
